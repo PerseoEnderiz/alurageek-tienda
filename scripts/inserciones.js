a99a6ee3c33e3dc8e6868800d98023788d18cb9f
Perseo Enderiz <perseoheroico@gmail.com>
2023-09-08 16:45:45 -0600

    pagina todos los productos añadida

--- scripts/inserciones.js
+++ scripts/inserciones.js
@@ -18,7 +18,7 @@ const imagenLogo = /*html*/`
     </div>
 `;
 const btnLogin = /*html*/`
-    <button class="btn_login btn">Login</button>
+    <button type="button" class="btn_login btn">Login</button>
 `;
 const busqueda = /*html*/`
     <svg class="busqueda" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>    
@@ -57,18 +57,20 @@ const menu = /*html*/`
 </div>  
 `
 const contacto = /*html*/`
-<div class="contacto">
-    <div class="contacto_form">
-        <h2 class="contacto_titulo">Hable con nosotros</h2>
-        <form action="enviar.php" method="post" class="Formulario">
+<div class="formulario">
+    <div class="formulario_contenido">
+        <h2 class="formulario_titulo">Hable con nosotros</h2>
+        <form action="enviar.php" method="post">
             <div class="campo_texto">
-                <input type="text" id="nombre" name="nombre" required class="inp">
+                <input type="text" id="nombre" name="nombre" required class="inp" placeholder="Aqui va el nombre">
                 <label for="nombre" class="lbl"><span class="text-nomb">Nombre</span></label>
+                <span class="inp-msg-error">Este campo es requerido</span>
             </div>
             <br>
             <div class="campo_texto">
                 <textarea id="mensaje" name="mensaje" required class="inp_textarea"></textarea>
                 <label for="mensaje" class="lbl"><span class="text-nomb">Mensaje</span></label>
+                <span class="inp-msg-error">Este campo es requerido</span>
             </div>
             <br>
             <input class="btn btn_principal" type="submit" value="Enviar Mensaje">
